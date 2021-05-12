6059bc86973b2d6c51953a3c7280fd3926184775
Shrivastava <ankitashrivasta5@deloitte.com>
2021-05-12 14:41:01 +0530

    after dockerize application and compose up

--- src/main/java/com/movie/netflixdata/controller/FetchDataFromDb.java
+++ src/main/java/com/movie/netflixdata/controller/FetchDataFromDb.java
@@ -2,6 +2,8 @@ package com.movie.netflixdata.controller;
 
 import com.movie.netflixdata.models.ContentModel;
 import com.movie.netflixdata.repository.FetchData;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -13,6 +15,7 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
@@ -21,9 +24,12 @@ import java.util.List;
 import java.util.Locale;
 import java.util.stream.Collectors;
 
+import static org.hibernate.tool.schema.SchemaToolingLogging.LOGGER;
+
 @RestController
 public class FetchDataFromDb {
-    private static final String  FILE_PATH = "src/main/resources/static/netflix_titles.csv";
+    private static final Logger LOGGER = LogManager.getLogger(FetchDataFromDb.class);
+    private static final String  FILE_PATH = "src/main/resources/data/netflix_titles.csv";
     @Autowired
     FetchData fetchData;
 
@@ -76,52 +82,105 @@ public class FetchDataFromDb {
 
     @GetMapping(path="/tvshows", params = "count", produces = "application/json")
 
-    List<ContentModel> getNumData( @RequestParam Integer count){
-
-        return  fetchData.findAll().stream()
-                .limit(count).collect(Collectors.toList());
-
+   public List<ContentModel> getNumData( HttpServletRequest request,
+                                   HttpServletResponse response, @RequestParam Integer count){
+        Instant startTime,endTime;
+        startTime = Instant.now();
+        List<ContentModel> netflixDataService=new ArrayList<>();
+        try{
+            netflixDataService=  fetchData.findAll().stream()
+                           .limit(count).collect(Collectors.toList());
+        }catch(Exception e){
+            LOGGER.error("Error is "+e.getMessage());
+        }
+        endTime = Instant.now();
+        long time = Duration.between(startTime,endTime).toMillis();
+        LOGGER.info("Process of execution of the request :"+time+" milliseconds .");
+        response.setHeader("X-TIME-TO-EXECUTE", String.valueOf(time));
+       return netflixDataService;
     }
 
     @GetMapping(path="/tvshows",params = "movieType", produces = "application/json")
-    List<ContentModel> getDataListed(@RequestParam String movieType){
-        return  fetchData.findAll().stream()
-                .filter(s->s.getListed_in().contains(movieType))
-                .collect(Collectors.toList());
+    List<ContentModel> getDataListed(HttpServletRequest request,
+                                     HttpServletResponse response, @RequestParam String movieType){
+        Instant startTime,endTime;
+        startTime = Instant.now();
+        List<ContentModel> netflixDataService=new ArrayList<>();
+        try{
+            netflixDataService=  fetchData.findAll().stream()
+                    .filter(s->s.getListed_in().contains(movieType))
+                    .collect(Collectors.toList());
+        }catch(Exception e){
+            LOGGER.error("Error is "+e.getMessage());
+        }
+        endTime = Instant.now();
+        long time = Duration.between(startTime,endTime).toMillis();
+        LOGGER.info("Process of execution of the request :"+time+" milliseconds .");
+        response.setHeader("X-TIME-TO-EXECUTE", String.valueOf(time));
+        return netflixDataService;
 
     }
 
     @GetMapping(path="/tvshows",params = "country", produces = "application/json")
-    List<ContentModel> getDataBasedOnCountry(@RequestParam String country){
-        return  fetchData.findAll().stream()
-                .filter(s->s.getCountry().contains(country))
-                .collect(Collectors.toList());
+    List<ContentModel> getDataBasedOnCountry(HttpServletRequest request,
+                                             HttpServletResponse response,@RequestParam String country){
+        Instant startTime,endTime;
+        startTime = Instant.now();
+        List<ContentModel> netflixDataService=new ArrayList<>();
+        try{
+            netflixDataService=  fetchData.findAll().stream()
+                    .filter(s->s.getCountry().contains(country))
+                    .collect(Collectors.toList());
+        }catch(Exception e){
+            LOGGER.error("Error is "+e.getMessage());
+        }
+        endTime = Instant.now();
+        long time = Duration.between(startTime,endTime).toMillis();
+        LOGGER.info("Process of execution of the request :"+time+" milliseconds .");
+        response.setHeader("X-TIME-TO-EXECUTE", String.valueOf(time));
+        return netflixDataService;
+
 
     }
 
     @RequestMapping(path="/tvshows",params ={ "startDate", "endDate"}, produces = "application/json")
-    List<ContentModel> getDataBasedOnDateRange(@RequestParam String startDate, @RequestParam String endDate ) throws ParseException {
+    List<ContentModel> getDataBasedOnDateRange(HttpServletRequest request,
+                                               HttpServletResponse response,@RequestParam String startDate, @RequestParam String endDate ) throws ParseException {
+        Instant startTime,endTime;
+        startTime = Instant.now();
+        List<ContentModel> netflixDataService=new ArrayList<>();
+
         DateFormat dateFormat = new SimpleDateFormat("MMMM d, yyyy", Locale.ENGLISH);
         LocalDate start= dateFormat.parse(startDate).toInstant().
                 atZone(ZoneId.systemDefault()).toLocalDate();
         LocalDate end = dateFormat.parse(endDate).toInstant()
                 .atZone(ZoneId.systemDefault()).toLocalDate();
-        return  fetchData.findAll().stream()
-                .filter(movieRow ->
-                {
-                    try {
-                        if (movieRow.getDate_added().trim().equals("")) {
-                            return false;
+        try{
+            netflixDataService=  fetchData.findAll().stream()
+                    .filter(movieRow ->
+                    {
+                        try {
+                            if (movieRow.getDate_added().trim().equals("")) {
+                                return false;
+                            }
+                            LocalDate dateAdded = dateFormat.parse(movieRow.getDate_added().replaceAll("\"", "").trim())
+                                    .toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+                            return dateAdded.isAfter(start) && dateAdded.isBefore(end);
+                        } catch (ParseException e) {
+                            e.printStackTrace();
                         }
-                        LocalDate dateAdded = dateFormat.parse(movieRow.getDate_added().replaceAll("\"", "").trim())
-                                .toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-                        return dateAdded.isAfter(start) && dateAdded.isBefore(end);
-                    } catch (ParseException e) {
-                        e.printStackTrace();
-                    }
-                    return false;
-                })
-                .collect(Collectors.toList());
+                        return false;
+                    })
+                    .collect(Collectors.toList());
+        }catch(Exception e){
+            LOGGER.error("Error is "+e.getMessage());
+        }
+        endTime = Instant.now();
+        long time = Duration.between(startTime,endTime).toMillis();
+        LOGGER.info("Process of execution of the request :"+time+" milliseconds .");
+        response.setHeader("X-TIME-TO-EXECUTE", String.valueOf(time));
+        return netflixDataService;
+
 
     }
 }
